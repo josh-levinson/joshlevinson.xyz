4128d7473e1be4bbb5f3975ca744e0ae27c46464
Josh Levinson <joshua.levinson@gmail.com>
2022-12-04 20:47:27 -0500

    Run jekyll build

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "What's Jekyll?",
+        "excerpt":"Jekyll is a static site generator, an open-source tool for creating simple yet powerful websites of all shapes and sizes. From the project’s readme: Jekyll is a simple, blog aware, static site generator. It takes a template directory […] and spits out a complete, static website suitable for serving with...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2013/12/31/whats-jekyll.html",
+        "teaser": null
+      },{
+        "title": "Example content",
+        "excerpt":"Howdy! This is an example blog post that shows several types of HTML content supported in this theme. Inline HTML elements HTML defines a long list of available inline tags, a complete list of which can be found on the Mozilla Developer Network. To bold text, use &lt;strong&gt;. To italicize...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2014/01/01/example-content.html",
+        "teaser": null
+      },{
+        "title": "Introducing Celeste",
+        "excerpt":"Celeste is a lightweight Jekyll theme that features a minimalist, content-first design. It places your content center stage and lets your readers view them in a clutter-free environment without visual distractions. It is based on Poole, the Jekyll butler, by @mdo. Built on Poole Poole is the Jekyll Butler, serving...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2015/06/14/introducing-celeste.html",
+        "teaser": null
+      }]
